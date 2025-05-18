231272f12b9e3d9e0f9971cfe93d5a45106a3a37
NINGauw <haonam.vt01@gmail.com>
2025-05-18 22:32:30 +0700

    Update the supplier
    Completed the supplier function and import GUI asset (Not create menu yet)

--- Militiplication/Assets/Script/Supply.cs
+++ Militiplication/Assets/Script/Supply.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class Supply : MonoBehaviour
+public class SupplyHealth : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int maxHealth = 20;
+    private int currentHealth;
+
+    public TextMeshPro healthText; // Gắn UI text hiển thị máu vào đây
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        UpdateHealthText();
     }
 
-    // Update is called once per frame
-    void Update()
+    void UpdateHealthText()
     {
-        
+        if (healthText != null)
+        {
+            healthText.text = $"{currentHealth} / {maxHealth}";
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            currentHealth = Mathf.Max(0, currentHealth - 1);
+            UpdateHealthText();
+
+            Destroy(other.gameObject); // Hủy đạn
+
+            if (currentHealth <= 0)
+            {
+                Debug.Log("Supply destroyed!");
+                ActiveSupply();
+            }
+        }
+    }
+
+    private void ActiveSupply(){
+        null;
     }
 }
