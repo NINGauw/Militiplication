e9f9c19f56a9a054b4981cb8b3a41954817eed40
NINGauw <haonam.vt01@gmail.com>
2025-05-19 22:16:08 +0700

    Add Menu pannel
    Add menu UI (Not finnish)

--- Militiplication/Assets/Script/Supply.cs
+++ Militiplication/Assets/Script/Supply.cs
@@ -40,6 +40,6 @@ public class SupplyHealth : MonoBehaviour
     }
 
     private void ActiveSupply(){
-        null;
+        
     }
 }
---
+++ Militiplication/Assets/Script/UIManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject mainMenuPanel;
+    public GameObject settingPanel;
+
+    public Button startGameButton;
+    public Button settingButton;
+    public Button shopButton;
+
+    private void Start()
+    {
+        // Gán sự kiện cho các nút
+        startGameButton.onClick.AddListener(OnStartGameClicked);
+        settingButton.onClick.AddListener(OnSettingClicked);
+        shopButton.onClick.AddListener(OnBackClicked);
+
+        // Mặc định hiển thị main menu
+        ShowMainMenu();
+    }
+
+    void OnStartGameClicked()
+    {
+        // Load scene chơi game
+        SceneManager.LoadScene("Game");
+    }
+
+    void OnSettingClicked()
+    {
+        // Hiện panel setting
+        mainMenuPanel.SetActive(false);
+        settingPanel.SetActive(true);
+    }
+
+    void OnBackClicked()
+    {
+        // Trở lại menu chính
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        settingPanel.SetActive(false);
+    }
+}
